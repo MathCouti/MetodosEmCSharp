25dcf5fff9daba5d87f8cc5090e3f86b0d64313d
MathCouti <mathcoutipro@gmail.com>
2022-07-28 20:19:54 -0300

    Adicionando os arquivos pela primeira vez

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Classe2
+{
+    class Program
+    {
+        //1. Crie um programa que contenha um método para realizar o cálculo da área de um círculo. A área é definida pela fórmula A = π * R². O usuário informará o valor do raio (R). Considere π igual a 3,14.
+
+        static void Main(string[] args)
+        {
+            double area;
+            double raio;
+           
+            Console.WriteLine("Qual é o RAIO do Triangulo?");
+            raio = double.Parse(Console.ReadLine());
+            area = calcArea(raio);
+            Console.WriteLine(area);
+
+        }
+        static double calcArea(double num) //método que calcula a área
+        {
+            double pi = 3.14;
+            double resultarea = pi * (num*num);
+            return resultarea;
+        }
+    }
+}
+
